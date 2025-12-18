6b3227ad789feb4f526b73f989acbdd8e689cdef
yaekobbundure <jackmanbonke@gmail.com>
2025-12-19 01:27:32 +0300

    schema edited

--- backend/src/modules/auth/auth.schema.ts
+++ backend/src/modules/auth/auth.schema.ts
@@ -1,19 +1,18 @@
+import { Role } from "@prisma/client";
 import { email, z } from "zod";
 
 
 export const registerSchema = z.object({
     first_name: z.string().min(2),
     last_name: z.string().min(2),
-    email: z.string().email(),
     phone: z.string().min(10),
-    location: z.string().min(2),
+    location: z.string().optional(),
+    email: z.string().email(),
     password: z.string().min(8),
-    role: z.enum(["FARMER", "BUYER"]).optional(),
+    role: z.enum(["Farmer", "Buyer"])
 });
-
 
 export const loginSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(8)
+    password: z.string().min(2),
 });
-
